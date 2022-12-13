9ff0a8e881cb8fa14a2a62d0cb74abd4aea1af56
z1nl0x <paulo.kreft@gmail.com>
2022-12-12 22:20:10 -0300

    Testing new feature

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -7,6 +7,14 @@ import { useSession, signOut } from "next-auth/react";
 const Navbar = () => {
   const { data: session, status } = useSession();
 
+  if (status === "loading") {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   let content = (
     <ul>
       <li>
